c82e5f3ae2f7df102663afc4bc541bfd0d208660
visualstudio <visualstudio@122-PC38>
2019-04-12 19:00:52 -0400

    TextAnalysistools

--- TextAnalysisTools/Program.cs
+++ TextAnalysisTools/Program.cs
@@ -10,6 +10,44 @@ namespace TextAnalysisTools
     {
         static void Main(string[] args)
         {
+            Program a = new Program();
+            Console.WriteLine("the longest word is :");
+            
+            Console.WriteLine(a.MyFunctionA("Ada Lovelace wrote the first algorithm designed for processing by an Analytical Engine."));
+
         }
+        
+       public string MyFunctionA(string input)
+        {// Write a c# program to find the longest word in a string.
+            string[] words = input.Split();
+
+            int wordsArrayLength = words.Length;
+            int[] wordsLength = new int[wordsArrayLength];
+
+            for(int i=0; i<words.Length;i++)
+            {
+                wordsLength[i] = words[i].Length;
+            }
+            // loop post condition: we now have array wordsLength which
+            // contains the length of each word
+
+            string currentWord = words[0];
+            string nextWord;
+
+            for(int y =0; y< words.Length-1;y++)
+            {
+                currentWord = words[y];
+                nextWord = words[y + 1];
+                
+            }
+
+           
+            return currentWord;
+
+        }
+
+        
+        
+        
     }
 }
